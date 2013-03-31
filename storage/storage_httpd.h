4679dde47ff8e6de580ffc0f65a1019e8f9bd605
huangkq <huangkq1989@gmail.com>
2013-03-31 11:18:04 +0800

    commented storage_http.*

---
+++ storage/storage_httpd.h
@@ -0,0 +1,32 @@
+/**************************************************
+ * 中文注释by: huangkq1989
+ * email: huangkq1989@gmail.com
+ *
+ * 启动http服务，提供http模式的下载
+ *
+ **************************************************/
+
+/**
+ * Copyright (C) 2008 Happy Fish / YuQing
+ *
+ * FastDFS may be copied only under the terms of the GNU General
+ * Public License V3, which may be found in the FastDFS source kit.
+ * Please visit the FastDFS Home Page http://www.csource.org/ for more detail.
+ **/
+
+//storage_httpd.h
+
+#ifndef _STORAGE_HTTPD_H_
+#define _STORAGE_HTTPD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    int storage_httpd_start(const char *bind_addr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
